9a8b51364317e00a05f59eb98405b8396e45f487
Nektarios <nektario@gmail.com>
2019-04-07 10:41:44 -0400

    Refactor tests to work with new global test state

--- functions/__tests__/integration/users.spec.js
+++ functions/__tests__/integration/users.spec.js
@@ -5,32 +5,18 @@
  * Tests for the /users route
  * 
  */
-
+const { validToken, user1, user2} = require('../../test-global-data')
 const request = require('supertest')
 const app = require('../../app')
 const User = require('../../users/user')
-// this is the mocked version of firebase-admin
-const db = require('firebase-admin').firestore()
+const firebaseHelper = require('../../helpers/firebase-helper')
 jest.mock('../../helpers/firebase-helper')
 
-const validToken = "1234567890"
-// This is the user that is being added to req.user during token validation
-const user = {
-    uid: "user-id1",
-    name: "John Doe",
+const user3 = User({
+    uid: "user-id3",
+    name: "Bob Smith",
     photoURL: "https://pbs.twimg.com/profile_images/1055263632861343745/vIqzOHXj.jpg",
-    email: "johndoe@fake-email.com"
-}
-const user2 = {
-    uid: "user-id2",
-    name: "Jane Doe",
-    photoURL: "https://pbs.twimg.com/profile_images/1055263632861343745/vIqzOHXj.jpg",
-    email: "janedoe@fake-email.com"
-}
-
-beforeEach(() => {
-    // clear the users from the "db"
-    db.clear()
+    email: "bob.smith@fake-email.com"
 })
 
 async function createUser(userToCreate) {
@@ -46,36 +32,38 @@ async function createUser(userToCreate) {
 }
   
 test('POST /users should create a new user', async () => {
-    const response = await createUser(user)
-    expect(response.body.uid).toBe(user.uid)
-    expect(response.body.name).toBe(user.name)
-    expect(response.body.photoURL).toBe(user.photoURL)
-    expect(response.body.email).toBe(user.email)
+    firebaseHelper.setRequestingUser(user3)
+    
+    const response = await createUser(user3.toJson())
+    expect(response.body.uid).toBe(user3.uid)
+    expect(response.body.name).toBe(user3.name)
+    expect(response.body.photoURL).toBe(user3.photoURL)
+    expect(response.body.email).toBe(user3.email)
     expect(response.body.created).toBeDefined()
 })
 
 test('GET /users/:uid should return a valid user object', async () => {
-    // First create a user and then try to read it back.
-    await createUser(user)
+    firebaseHelper.setRequestingUser(user1)
+
     return request(app)
-        .get('/users/' + user.uid)
+        .get('/users/' + user1.uid)
         .set('Authorization', 'Bearer ' + validToken)
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200)
         .then(response => {
-            expect(response.body.uid).toBe(user.uid)
-            expect(response.body.name).toBe(user.name)
-            expect(response.body.photoURL).toBe(user.photoURL)
-            expect(response.body.email).toBe(user.email)
+            expect(response.body.uid).toBe(user1.uid)
+            expect(response.body.name).toBe(user1.name)
+            expect(response.body.photoURL).toBe(user1.photoURL)
+            expect(response.body.email).toBe(user1.email)
             expect(response.body.created).toBeDefined()
         })
 })
 
 test('GET /users/:uid where requesting user != requested user, private data such as email should not returned', async () => {
-    // First create a user and then try to read it back.
-    await createUser(user2)
+    firebaseHelper.setRequestingUser(user1)
+
     return request(app)
         .get('/users/' + user2.uid)
         .set('Authorization', 'Bearer ' + validToken)
@@ -94,8 +82,10 @@ test('GET /users/:uid where requesting user != requested user, private data such
 })
 
 test('GET /users/:invalidId should return 404', () => {
+    firebaseHelper.setRequestingUser(user1)
+
     return request(app)
-        .get('/users/userIdInvalid')
+        .get('/users/invalidUserId')
         .set('Authorization', 'Bearer ' + validToken)
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json')
@@ -108,13 +98,12 @@ test('GET /users/:invalidId should return 404', () => {
 })
 
 test('PUT /users/:uid should update the user', async () => {
-    // First create a user and then try to update the photoURL and add an about section
+    firebaseHelper.setRequestingUser(user1)
     const newPhotoURL = "http://photourl..."
     const about = "About me"
 
-    await createUser(user)
     return request(app)
-        .put('/users/' + user.uid)
+        .put('/users/' + user1.uid)
         .set('Authorization', 'Bearer ' + validToken)
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json')
@@ -125,10 +114,10 @@ test('PUT /users/:uid should update the user', async () => {
         .expect('Content-Type', /json/)
         .expect(200)
         .then(response => {
-            expect(response.body.uid).toBe(user.uid)
-            expect(response.body.name).toBe(user.name)
+            expect(response.body.uid).toBe(user1.uid)
+            expect(response.body.name).toBe(user1.name)
             expect(response.body.photoURL).toBe(newPhotoURL)
-            expect(response.body.email).toBe(user.email)
+            expect(response.body.email).toBe(user1.email)
             expect(response.body.about).toBe(about)
             expect(response.body.created).toBeDefined()
         })
