af04c248c74343c7412fe4151c7cb0452472ce06
Nektarios <nektario@gmail.com>
2019-04-09 04:14:42 -0400

    Creating api/db connection

---
+++ client/src/api.js
@@ -0,0 +1,26 @@
+import config from './config'
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+firebase.initializeApp(config.firebase)
+const db = firebase.firestore()
+const apiUrl = config.backendUrl
+
+/**
+ * Gets a list of Help Requests directly from the database. 
+ * Also sets up a listener to get realtime updates and calls the callback with the 
+ *  full list every time the dataset changes
+ * @param {function} callback - The function to call with the list of help requests
+ */
+export function getHelpRequests(callback) {
+    db.collection('help-requests').where('status', '==', 'active')
+        .onSnapshot(querySnapshot => {
+            const helpRequests = []
+
+            querySnapshot.forEach(doc => {
+                const helpRequest = doc.data()
+                helpRequests.push(helpRequest)
+            })
+            callback(helpRequests)
+    })
+}
