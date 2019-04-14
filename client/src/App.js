074f50850e1e3d70e8881911d34cde9c5f646514
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-04-14 16:53:07 -0400

    add welcome to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
-import Login from './components/Forms/Login'
+import Welcome from './pages/Welcome/Welcome'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.handleLoginClick = this.handleLoginClick.bind(this)
+    this.handleCreateClick = this.handleCreateClick.bind(this)
+  }
+
+  handleLoginClick() {
+    console.log('login clicked');
+  }
+  handleCreateClick() {
+    console.log('create account clicked');
+  }
+
   render() {
     return (
-      <div className="App">
-        <Login />
+      <div className="app">
+        <Welcome 
+          onLoginClick={this.handleLoginClick} 
+          onCreateClick={this.handleCreateClick} 
+        />
       </div>
     );
   }
