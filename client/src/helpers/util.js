f2699e616a41a58b5876fce65535ac4f7acae510
Nektarios <nektario@gmail.com>
2019-05-08 21:51:39 -0400

    Helper to create UID for conversation documents

--- client/src/helpers/util.js
+++ client/src/helpers/util.js
@@ -8,3 +8,13 @@ export function getDisplayName(name) {
 
     return `${firstName} ${lastInitial}`
 }
+
+// Conversation document IDs will be made up of both user uids: <smaller uid>-<larger uid>.
+// This way we can always find a thread between 2 users just by its document ID
+export function createConversationUidFromUserUids(uid1, uid2) {
+    if (uid1 < uid2) {
+        return `${uid1}-${uid2}`
+    } else {
+        return `${uid2}-${uid1}`
+    }
+}
