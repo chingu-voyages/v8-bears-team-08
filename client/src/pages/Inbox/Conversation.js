0cbbad75bce54d728d9754dcddf1b051cd713449
Nektarios <nektario@gmail.com>
2019-05-10 00:27:54 -0400

    More conversation code & styling

--- client/src/pages/Inbox/Conversation.js
+++ client/src/pages/Inbox/Conversation.js
@@ -1,46 +1,70 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import Avatar from '../../components/Avatar'
 import './Conversation.scss'
 import * as firebase from '../../helpers/firebase'
 import * as api from '../../api'
+import { LoggedInUserContext } from '../../App'
+import conversationFactory from './conversation-factory'
 
+/*
+ *  This component is for a direct conversation between the logged in user and another user.
+ *  This will be used as the communication when contacting a user to provide assistance on a Help Request.
+ *  The user can navigate here via:
+ *      1. Inbox -> Clicking on the conversation; props.location.state.conversationDetails will be populated with the conversation details
+ *      2. Clicking the "Send a Message" button on a Help Request; props.location.state.messageRecipient will be populated with the receiving user's profile info.
+ *          a. This will require a query to retrieve the conversation information (or create a new conversation if there isn't one already.)
+ */
 function Conversation(props) {
-    const [conversation, setConversation] = useState(props.location.state ? props.location.state.conversation : undefined)
-    const [messages, setMessages] = useState([])
+    const [conversationDetails, setConversationDetails] = useState(undefined)
+    const [receivingUser, setReceivingUser] = useState(undefined)
+    const [conversationMessages, setConversationMessages] = useState([])
     const [messageBoxText, setMessageBoxText] = useState("")
     const [unsubscribe, setUnsubscribe] = useState(undefined)
+    const loggedInUser = useContext(LoggedInUserContext)
     const conversationUid = props.match.params.uid
 
-    useEffect(function getConversationDetails() {
-        if (props.location.state.conversation) {
-            console.log('already have conversation details')
-            setConversation(props.location.state.conversation)
-        } else {
-            console.log('getting conversation details')
-            api.getConversationDetails(conversationUid)
-                .then(response => {
-                    if (response.data) {
-                        setConversation(response.data)
-                    } else {
-                        console.log('need to create conversation')
 
-                    }
-                })
+    function getReceivingUserFromConversation(conversation) {
+        return conversation.users.filter(user => user.uid != loggedInUser.uid)[0]
+    }
+
+    useEffect(function getConversationDetails() {
+        if (isViewValid()) {
+            if (props.location.state.conversationDetails) {
+                console.log('already have conversation details')
+                setReceivingUser(getReceivingUserFromConversation(props.location.state.conversationDetails))
+                setConversationDetails(props.location.state.conversationDetails)
+            } else {
+                console.log('getting conversation details')
+                api.getConversationDetails(conversationUid)
+                    .then(response => {
+                        if (response.data) {
+                            setConversationDetails(response.data)
+                            setReceivingUser(getReceivingUserFromConversation(response.data))
+                        } else {
+                            // there is no conversation created yet.  
+                            // It needs to be created when the first message is sent.
+                        }
+                    })
+            }
         }
     }, [])
 
     useEffect(function subscribeToConversationMessages() {
-        api.subscribeToConversationMessages(conversationUid, 
-            response => {
-                if (response.messages.length > 0) {
-                    console.log('has messages')
-                    setMessages(response.messages)
-                } else {
-                    console.log('no messages')
-                }
-            },
-            errorResponse => {
-                
-            })
+        if (isViewValid()) {
+            api.subscribeToConversationMessages(conversationUid, 
+                response => {
+                    if (response.messages.length > 0) {
+                        console.log('has messages')
+                        setConversationMessages(response.messages)
+                    } else {
+                        console.log('no messages')
+                    }
+                },
+                errorResponse => {
+                    
+                })
+        }
     }, [])
 
     async function sendMessage(e) {
@@ -51,32 +75,45 @@ function Conversation(props) {
         const message = {
             created: new Date().toISOString(),
             text: messageText,
-            senderUid: firebase.getUser().uid,
-            receiverUid: ""
+            senderUid: loggedInUser.uid,
+            receiverUid: undefined
         }
         
-        // if we have no conversation in state, then the first message we send should also create the conversation.
-        if (!conversation) {
-            const response = await api.createConversation(conversationUid)
-            message.receiverUid = response.users[0].uid
-            setConversation(response)
+        // if we have no conversation in state, then we should create the conversation before sending the first message.
+        if (!conversationDetails) {
+            const response = await api.createConversation(conversationUid, conversationFactory(props.location.state.messageRecipient, loggedInUser))
+            message.receiverUid = props.location.state.messageRecipient.uid
+            setReceivingUser(props.location.state.messageRecipient)
+            setConversationDetails(response)
         } else {
-            message.receiverUid = conversation.users[0].uid
+            message.receiverUid = receivingUser.uid
         }
         
-        console.log(message)
         api.sendMessage(conversationUid, message)
+    }
+
+    function isViewValid() {
+        return conversationDetails || (props.location.state && props.location.state.messageRecipient)
+    }
+
+    if (!isViewValid()) {
+        // view is being accessed outside of normal flow as we have no valid chat recipient or conversation
+        return <h1>404</h1>
     }
 
     return (
         <div className='conversation__container d-flex flex-col'>
-            <h1>Conversation { conversation && <span>with {conversation.users[0].name}</span>}</h1>
+            <h1>Conversation { receivingUser && <span>with {receivingUser.name}</span>}</h1>
             
             <div className='conversation__messages-container'>
                 <div className='conversation__messages'>
-                    { messages && messages.map(message => (
-                        <div key={message.uid}>{message.text}</div>
-                    ))}
+                    <ul>
+                        { conversationDetails && conversationMessages && conversationMessages.map(message => {
+                            message.photoURL = conversationDetails.users.filter(user => user.uid == message.senderUid)[0].photoURL
+                            message.isMyMessage = message.senderUid === loggedInUser.uid
+                            return <Message key={message.uid} message={message} />
+                        })}
+                    </ul>
                 </div>
             </div>
 
@@ -85,10 +122,24 @@ function Conversation(props) {
                     className='conversation__input-box'
                     type='text'
                     value={messageBoxText}
+                    placeholder='message'
                     onChange={e => setMessageBoxText(e.target.value)}
                     />
             </form>
         </div>
+    )
+}
+
+function Message({ message }) {
+    // the flex-row-reverse & flex-end combo of classes below are so that the received messages show on the left of the screen with the avatar on the left
+    return (
+        <li 
+            key={message.uid}
+            className={message.isMyMessage ? 'conversation__message d-flex flex-row flex-end' : 'conversation__message d-flex flex-row-reverse flex-end'}
+        >
+            <div className='conversation__message-text'>{message.text}</div>
+            <Avatar url={message.photoURL} size='small' />
+        </li>
     )
 }
 
