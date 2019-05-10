f436cacc1e56f68d46066d97d55dfb8ce9122c41
Nektarios <nektario@gmail.com>
2019-05-10 00:28:15 -0400

    Use new loggedInUser context

--- client/src/pages/Home/HelpRequestDetails.js
+++ client/src/pages/Home/HelpRequestDetails.js
@@ -1,19 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import * as firebase from '../../helpers/firebase'
 import * as util from '../../helpers/util'
+import { LoggedInUserContext } from '../../App'
 
 function HelpRequestDetails(props) {
+    const loggedInUser = useContext(LoggedInUserContext)
     console.log('HRD props', props)
     const helpRequest = props.location.state
-    const conversationUid = util.createConversationUidFromUserUids(helpRequest.user.uid, firebase.getUser().uid)
+    const conversationUid = util.createConversationUidFromUserUids(helpRequest.user.uid, loggedInUser.uid)
 
     return (
         <div>
             <Link 
                 to={{ 
                         pathname: `/inbox/${conversationUid}`, 
-                        state: { user: helpRequest.user }
+                        state: { messageRecipient: helpRequest.user }
                     }}>Send Message</Link>
 
             <h1>{helpRequest && helpRequest.title}</h1>
