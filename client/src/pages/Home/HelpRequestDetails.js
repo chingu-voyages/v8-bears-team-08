b619ffb323f84a60a7dd73832a1352adad208eba
Nektarios <nektario@gmail.com>
2019-05-28 06:32:45 -0400

    Refactor some components to avoid using context

--- client/src/pages/Home/HelpRequestDetails.js
+++ client/src/pages/Home/HelpRequestDetails.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import LinkButton from '../../components/LinkButton'
 import * as util from '../../helpers/util'
