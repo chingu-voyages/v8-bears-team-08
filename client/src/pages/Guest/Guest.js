1d8698eee475875b37fcb98fad17763df15927d6
Nektarios <nektario@gmail.com>
2019-06-05 22:33:39 -0400

    Fixed route handling when logged in as guest

--- client/src/pages/Guest/Guest.js
+++ client/src/pages/Guest/Guest.js
@@ -7,7 +7,7 @@ function Guest(props) {
         <div className='guest'>
             <div className='title'>
                 <p>You must be a registered user to access this content.</p>
-                <p>Please <a href={props.location.state.referrer || '/'} onClick={() => firebase.signOut()}>login or create an account</a>.</p>
+                <p>Please <a href={props.referrer || '/'} onClick={() => firebase.signOut()}>login or create an account</a>.</p>
             </div>
         </div>
     )
