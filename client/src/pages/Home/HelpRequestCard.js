1e08d892a68d4a60c209b8330faaf08076310c22
Nektarios <nektario@gmail.com>
2019-06-12 23:23:32 -0400

    Tweak card display sizes; fixes #38

--- client/src/pages/Home/HelpRequestCard.js
+++ client/src/pages/Home/HelpRequestCard.js
@@ -15,35 +15,33 @@ function HelpRequestCard({ cardSize, helpRequest }) {
     if (helpRequest.neededAsap) {
         neededAt = 'ASAP'
     } else {
-        neededAt = util.getRelativeLocaleTime(new Date(helpRequest.neededDatetime))
+        neededAt = util.getRelativeCalendarTime(new Date(helpRequest.neededDatetime))
     }
 
     const tags = helpRequest.tags ? helpRequest.tags.slice(0, 2).join(', ') : ''
     
     if (cardSize === 'small') {
         return (
-            <li className='help-request-card'>
-                <div className='small-card'>
-                    <Link className='card-link' to={{ pathname: `/help-requests/${helpRequest.uid}`, state: helpRequest }}>
-                        <div className='info'>
-                            <div>
-                                <div className='secondary-text'>{displayName} needs</div>
-                                <div className='title-text'>{makeSentenceCase(helpRequest.title)}</div>
-                                <div className='secondary-text'>{neededAt}</div>
-                            </div>
+            <li className='small-card'>
+                <Link className='card-link' to={{ pathname: `/help-requests/${helpRequest.uid}`, state: helpRequest }}>
+                    <div className='info'>
+                        <div>
+                            <div className='secondary-text'>{displayName} needs</div>
+                            <div className='title-text'>{makeSentenceCase(helpRequest.title)}</div>
+                            <div className='secondary-text'>{neededAt}</div>
                         </div>
-                        
-                        <img
-                            className='help-request-list-item__photo'
-                            src={helpRequest.photoURL || helpRequest.user.photoURL}
-                            alt='Help Request'
-                            />
-                    </Link>
-                    
-                    <div className='bottom'>
-                        <div className='secondary-text'>posted {util.getRelativeTime(helpRequest.created)}</div>
-                        <div className='secondary-text'>{tags}</div>
                     </div>
+                    
+                    <img
+                        className='help-request-list-item__photo'
+                        src={helpRequest.photoURL || helpRequest.user.photoURL}
+                        alt='Help Request'
+                        />
+                </Link>
+                
+                <div className='bottom'>
+                    <div className='secondary-text'>posted {util.getRelativeTime(helpRequest.created)}</div>
+                    <div className='secondary-text'>{tags}</div>
                 </div>
             </li>
         )
@@ -51,40 +49,38 @@ function HelpRequestCard({ cardSize, helpRequest }) {
     // large card
     else {
         return (
-            <li className='help-request-card'>
-                <div className='big-card'>
-                    <Link className='header' to={{ pathname: `/help-requests/${helpRequest.uid}`, state: helpRequest }}>
-                        <img
-                            className='help-request-list-item__photo'
-                            src={helpRequest.photoURL || helpRequest.user.photoURL}
-                            alt='Help Request'
-                            />
+            <li className='big-card'>
+                <Link className='header' to={{ pathname: `/help-requests/${helpRequest.uid}`, state: helpRequest }}>
+                    <img
+                        className='help-request-list-item__photo'
+                        src={helpRequest.photoURL || helpRequest.user.photoURL}
+                        alt='Help Request'
+                        />
+                </Link>
+
+                <div className='info'>
+                    <Link to={{ pathname: `/help-requests/${helpRequest.uid}`, state: helpRequest }}>
+                        <div>
+                            <div className='secondary-text'>{displayName} needs</div>
+                            <div className='title-text'>{makeSentenceCase(helpRequest.title)}</div>
+                            <div className='secondary-text'>{neededAt}</div>
+                        </div>
+
+                        <div>
+                            <p className='description'>{helpRequest.description}</p>
+                        </div>
                     </Link>
 
-                    <div className='info'>
-                        <Link to={{ pathname: `/help-requests/${helpRequest.uid}`, state: helpRequest }}>
-                            <div>
-                                <div className='secondary-text'>{displayName} needs</div>
-                                <div className='title-text'>{makeSentenceCase(helpRequest.title)}</div>
-                                <div className='secondary-text'>{neededAt}</div>
-                            </div>
-
-                            <div>
-                                <p className='description'>{helpRequest.description}</p>
-                            </div>
-                        </Link>
-
-                        <div className='bottom'>
-                            <div className='d-flex flex-col'>
-                                <div className='secondary-text'>{tags}</div>
-                                <div className='secondary-text'>posted {util.getRelativeTime(helpRequest.created)}</div>
-                            </div>
-                            { conversationUid &&
-                                <LinkButton style='outlined' to={{ pathname: `/inbox/${conversationUid}`, state: { messageRecipient: helpRequest.user }}}>
-                                    Send message
-                                </LinkButton>
-                            }
+                    <div className='bottom'>
+                        <div className='d-flex flex-col'>
+                            <div className='secondary-text'>{tags}</div>
+                            <div className='secondary-text'>posted {util.getRelativeTime(helpRequest.created)}</div>
                         </div>
+                        { conversationUid &&
+                            <LinkButton style='outlined' to={{ pathname: `/inbox/${conversationUid}`, state: { messageRecipient: helpRequest.user }}}>
+                                Send message
+                            </LinkButton>
+                        }
                     </div>
                 </div>
             </li>
